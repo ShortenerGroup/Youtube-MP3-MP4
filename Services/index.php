e88d1d9c18f2ffe3de0f8ff82f582050d7e981d1
skali1 <infos@shortener.ma>
2017-07-05 20:16:50 +0200

    Update index.php

--- Services/index.php
+++ Services/index.php
@@ -3,26 +3,26 @@
     <meta name="robots" content="noindex">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
-<script type="text/javascript" src="/mp3/js/iframe.js"></script>
+<script type="text/javascript" src="js/iframe.js"></script>
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,700,700i"> 
-<link rel="stylesheet" type="text/css" href="/mp3/css/iframe.css">
+<link rel="stylesheet" type="text/css" href="css/iframe.css">
 
 </head>
 <body>
    <div id="mySidenav" class="sidenav">
       <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="/mp3/">Home</a>
-          <a href="/mp3/About" >About</a>
-            <a id="active" href="/mp3/Services">Services</a>
-              <a href="/mp3/Developers">Developers</a>
-                <a href="/mp3/Contact">Contact</a>
+        <a href="/">Home</a>
+          <a href="About" >About</a>
+            <a id="active" href="Services">Services</a>
+              <a href="Developers">Developers</a>
+                <a href="Contact">Contact</a>
    </div>
 
     <div id="main">
       <table style="width:100%">
           <tr>
              <th style="text-align:left;width:50%">
-               <a href="/mp3"><img src="/mp3/img/lmp3.png" width="80" height="30" /></a>
+               <a href="/"><img src="img/lmp3.png" width="80" height="30" /></a>
              </th>
              <th style="text-align:right;direction:rtl;width:50%;color:#fff">
                 <span class="curs" onclick="openNav()">&#9776;OPEN</span>
@@ -30,7 +30,7 @@
           </tr>
       </table>
 
- <iframe src="http://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="100%" height="360" scrolling="no" style="color:#fff;border:none;"></iframe>
+ <iframe src="https://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="100%" height="360" scrolling="no" style="color:#fff;border:none;"></iframe>
 
       <div style="background-color:#f0f0f0;border-radius:10px;padding-left:10px;direction:ltr;font-size:20px;margin:20px;">
 
