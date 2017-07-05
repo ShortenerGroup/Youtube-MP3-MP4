ff22fcad3d74d7cf7c851bcc70d02505dc04f4d3
skali1 <infos@shortener.ma>
2017-07-05 20:18:08 +0200

    Update index.php

--- Developers/index.php
+++ Developers/index.php
@@ -3,27 +3,27 @@
     <meta name="robots" content="noindex">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
-<script type="text/javascript" src="/mp3/js/iframe.js"></script>
-<script type="text/javascript" src="/mp3/js/link.js"></script>
+<script type="text/javascript" src="js/iframe.js"></script>
+<script type="text/javascript" src="js/link.js"></script>
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,700,700i"> 
-<link rel="stylesheet" type="text/css" href="/mp3/css/iframe.css">
+<link rel="stylesheet" type="text/css" href="css/iframe.css">
 
 </head>
 <body>
    <div id="mySidenav" class="sidenav">
       <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="/mp3/">Home</a>
-          <a href="/mp3/About" >About</a>
-            <a href="/mp3/Services">Services</a>
-              <a id="active" href="/mp3/Developers">Developers</a>
-                <a href="/mp3/Contact">Contact</a>
+        <a href="/">Home</a>
+          <a href="About" >About</a>
+            <a href="Services">Services</a>
+              <a id="active" href="Developers">Developers</a>
+                <a href="Contact">Contact</a>
    </div>
 
     <div id="main">
       <table style="width:100%">
           <tr>
              <th style="text-align:left;width:50%">
-               <a href="/mp3"><img src="/mp3/img/lmp3.png" width="80" height="30" /></a>
+               <a href="/"><img src="img/lmp3.png" width="80" height="30" /></a>
              </th>
              <th style="text-align:right;direction:rtl;width:50%;color:#fff">
                 <span class="curs" onclick="openNav()">&#9776;OPEN</span>
@@ -31,7 +31,7 @@
           </tr>
       </table>
 
- <iframe src="http://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="100%" height="360" scrolling="no" style="color:#fff;border:none;"></iframe>
+ <iframe src="https://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="100%" height="360" scrolling="no" style="color:#fff;border:none;"></iframe>
 
     <div style="background-color:#f0f0f0;border-radius:10px;padding-left:10px;direction:ltr;font-size:20px;margin:20px;">
 
@@ -50,16 +50,16 @@ Do you operate your own website? Do you want to give your users the opportunity 
 iFrame API</h2>
 <div>
 <pre class="code" style="background-color: #f4f8fc; border-radius: 2px; color: #0a7ebe; font-family: Roboto, sans-serif; line-height: 27px; padding: 10px 20px; text-align: initial; white-space: pre-wrap;"><span class="s" style="font-weight: 700;">Example [iframe]</span>
-&lt;iframe src="http://shortener.ma/iframe/" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
-<iframe src="http://shortener.ma/iframe/" width="600" height="360" scrolling="no" style="border:none;"></iframe>
+&lt;iframe src="https://shortener.ma/iframe/" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
+<iframe src="https://shortener.ma/iframe/" width="600" height="360" scrolling="no" style="border:none;"></iframe>
 
 <span class="s" style="font-weight: 700;">Example [iframe / mp3]</span>
-&lt;iframe src="http://shortener.ma/iframe/?<span class="s" style="font-weight: 700;">v</span>=<span class="i" style="font-style: italic;">gIYaTs1Kw90</span>&amp;<span class="s" style="font-weight: 700;">f</span>=<span class="i" style="font-style: italic;">mp3</span>" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
-<iframe src="http://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="600" height="360" scrolling="no" style="border:none;"></iframe>
+&lt;iframe src="https://shortener.ma/iframe/?<span class="s" style="font-weight: 700;">v</span>=<span class="i" style="font-style: italic;">gIYaTs1Kw90</span>&amp;<span class="s" style="font-weight: 700;">f</span>=<span class="i" style="font-style: italic;">mp3</span>" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
+<iframe src="https://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp3" width="600" height="360" scrolling="no" style="border:none;"></iframe>
 
 <span class="s" style="font-weight: 700;">Example [iframe / mp4]</span>
-&lt;iframe src="http://shortener.ma/iframe/?<span class="s" style="font-weight: 700;">v</span>=<span class="i" style="font-style: italic;">gIYaTs1Kw90</span>&amp;<span class="s" style="font-weight: 700;">f</span>=<span class="i" style="font-style: italic;">mp4</span>" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
-<iframe src="http://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp4" width="600" height="360" scrolling="no" style="border:none;"></iframe>
+&lt;iframe src="https://shortener.ma/iframe/?<span class="s" style="font-weight: 700;">v</span>=<span class="i" style="font-style: italic;">gIYaTs1Kw90</span>&amp;<span class="s" style="font-weight: 700;">f</span>=<span class="i" style="font-style: italic;">mp4</span>" width="600" height="360" scrolling="no" style="border:none;"&gt;&lt;/iframe&gt;
+<iframe src="https://shortener.ma/iframe/?v=gIYaTs1Kw90&f=mp4" width="600" height="360" scrolling="no" style="border:none;"></iframe>
 </pre>
 
 </div>
